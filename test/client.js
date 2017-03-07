c45cd786fc4cdd8564824c43ab34477f8b1921ca
Janggwan <limg00n@kaist.ac.kr>
2017-03-07 21:05:35 +0900

    outer link of jsmpg is modified

--- test/client.js
+++ test/client.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws')
-const ws = new WebSocket('ws://localhost:5000')
+const ws = new WebSocket('ws://192.168.1.9:5000')
 
 ws.on('open', () => {
   console.log('Connected to stream')
