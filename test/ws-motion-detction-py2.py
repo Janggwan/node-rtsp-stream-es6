0bf27175c514032a1f63d426c9e50e3bfa3e102d
Janggwan <limg00n@kaist.ac.kr>
2017-03-11 00:41:26 +0900

    script was renamed

--- test/ws-motion-detction-py2.py
+++ test/ws-motion-detction-py2.py
@@ -2,8 +2,18 @@ import websocket
 import thread
 import time
 
+from Queue import Queue
+
+frameQueue = Queue()
+
+
+
 def on_message(ws, message):
-    print message
+    #print message
+    print ("message received")
+    frameQueue.put_nowait(message)
+    print ("Msg was added to the queue")
+
 
 def on_error(ws, error):
     print error
@@ -21,12 +31,25 @@ def on_open(ws):
         print "thread terminating..."
     thread.start_new_thread(run, ())
 
+def worker():
+    while True:
+        item = frameQueue.get()
+        print ("display message :")
+        print (item)
+        frameQueue.task_done()
+
+
+
+
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://echo.websocket.org/",
+    ws = websocket.WebSocketApp("ws://192.168.1.9:5000",
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
     ws.on_open = on_open
+
+    thread.start_new_thread(worker, ())
+
     ws.run_forever()
